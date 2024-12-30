87d4154a879995e8cc43366c8a25bc57485f85b1
sgenov <genov.simeon86@googlemail.com>
2024-12-30 23:34:17 +0200

    remove alternative xPath for ShippingDetails

--- EbaySpecflow/Pages/ItemDetailsPage.cs
+++ EbaySpecflow/Pages/ItemDetailsPage.cs
@@ -18,7 +18,6 @@ namespace EbayMonopolyTests.Pages
 
         private IWebElement ItemTitle => _driver.FindElement(By.CssSelector("h1.x-item-title__mainTitle span"));
         private IWebElement ItemPrice => _driver.FindElement(By.CssSelector("div.x-price-primary span"));
-        //private IWebElement ShippingDetails => _driver.FindElement(By.XPath("//div[contains(@class, 'ux-labels-values__labels ') and contains(., 'Shipping:')]/following-sibling::div//button[child::span[text()='See details']]"));
         private IWebElement ShippingDetails => _driver.FindElement(By.XPath("//button[child::span[text()='See details' and child::span[text()='for shipping']]]"));
         private IWebElement CloseShippingDetails => _driver.FindElement(By.CssSelector("button[aria-label=\"Close window\"]"));
         private IWebElement CountryDropdown => _driver.FindElement(By.Id("shCountry"));
